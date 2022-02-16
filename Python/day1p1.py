448d07dc01c6ed41876944086ebdf49fd32c6d1b
Bla125 <51074685+Bla125@users.noreply.github.com>
2022-02-16 17:32:23 +0800

    Add files via upload

---
+++ Python/day1p1.py
@@ -0,0 +1,18 @@
+from zoautil_py import datasets
+
+
+input_data = datasets.read('Z00620.ADVINPUT(DAY1IN)')
+
+input_num = list(input_data.split())
+
+largercount = 0
+
+for i in range(0,len(input_num)):
+    j = i - 1
+    if int(input_num[i]) > int(input_num[j]):
+        largercount += 1
+
+print(largercount)
+
+with open(r'/z/z00620/advpython/output','w') as file:
+    file.write(str(largercount))
